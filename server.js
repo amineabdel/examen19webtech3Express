6556cb74852a8f6627a2fa4393742d733caca357
amineabdel <amine.oostende@hotmail.com>
2019-01-22 15:36:01 +0100

    alle studenten + search

--- server.js
+++ server.js
@@ -5,34 +5,50 @@ const cors = require('cors')
 const MongoClient = require('mongodb').MongoClient
 
 var db;
-/*
-MongoClient.connect('mongodb://localhost:27017/products', { useNewUrlParser: true },
- (err, database) => {
+url = 'mongodb://localhost:27017/examen';
+MongoClient.connect(url, { useNewUrlParser: true },
+  (err, database) => {
     if (err) return console.log(err)
-    db = database.db('products')
+    db = database.db('examen')
     app.listen(process.env.PORT || 4000, () => {
       console.log('Listening on port 4000')
     })
-})*/
+  })
 
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
-var originsWhitelist = ['http://localhost:4200'];
-
-var corsOptions = {
-  origin: function(origin, callback){
-        var isWhitelisted = originsWhitelist.indexOf(origin) !== -1;
-        callback(null, isWhitelisted);
-  },
-  credentials: true
-};
-
-app.use(cors(corsOptions));
 
 // Redirect to list
 app.get('/', (req, res) => {
-   res.send("weed");
+ 
+
+  db.collection('inhaal').find().toArray((err, resultaat) => {
+    console.log((resultaat));
+    res.render("index.ejs",{ex: (resultaat)})
+    //res.json(resultaat)
+  })
 })
 
+app.post('/formAanvraag', (req, res) => {
+  db.collection('inhaal').insertOne({ body: req.body, date: new Date() },{$exists: true}, (err, resultaat) => {
+    res.redirect("/");
+  })
+})
+
+app.get("/:stud", (req, res) => {
+  console.log(req.body);
+  
+  db.collection("inhaal").find().toArray( (err, resultaat) => {
+    res.render("index.ejs", { ex: resultaat })
+  })
+ 
+})
+
+app.post("/search", (req, res) => {
+  stud = req.body.student;
+    db.collection("inhaal").find(stud).toArray( (err, resultaat) => {
+      res.redirect("/" + stud);
+    })
+  });
 
